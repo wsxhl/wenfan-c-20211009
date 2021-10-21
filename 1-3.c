02abdd9494a36332365ee208e7328bc52b14e2d1
xhl <983209045@qq.com>
2021-10-21 21:23:52 +0800

    scanf 输入 int 定义变量

---
+++ 1-3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+   int price = 0;
+
+   printf("请输入价格：");
+   scanf("%d",&price);
+
+   int change = 100-price;
+
+    printf("找您%d元",change);
+
+
+
+    return 0;
+}//
+// Created by xhlly on 2021/10/19.
+//
+//
+// Created by xhlly on 2021/10/21.
+//
+
