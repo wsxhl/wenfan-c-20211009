ff63778cec3eac837c1f52ecadea4294c00c6726
xhl <983209045@qq.com>
2021-10-24 08:36:32 +0800

    输入两个变量 1、先定义变量int a 2、scanf输入前&符号 3、输入不确定的值用%d代替

---
+++ 2-1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a;
+    int b;
+    
+    printf("请输入两个数字：");
+    scanf("%d %d",&a,&b);
+
+    printf("%d + %d =%d\n",a,b,a+b);
+
+    return 0;
+}
+//
+// Created by xhlly on 2021/10/24.
+//
+
