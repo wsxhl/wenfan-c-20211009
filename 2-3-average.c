0288dc651fefe48591429d1c191efaa55e4175c7
xhl <983209045@qq.com>
2021-10-29 10:28:03 +0800

    1定义赋值a b 2 换行符号 3 scanf变量前加& 4.带小数点除数后带2.0

---
+++ 2-3-average.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a= 0;
+    int b= 0;
+
+    printf("请输入两个整数\n");
+    scanf("%d %d",&a,&b);
+
+    printf("%d和%d的平均数是 %f",a,b,(a+b)/2.0);
+
+    return 0;
+}//
+// Created by xhlly on 2021/10/29.
+//
+
