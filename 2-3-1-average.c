d586e71d76f918c7538d315409611fd0b87bba53
xhl <983209045@qq.com>
2021-10-29 10:40:42 +0800

    1、int定义更简洁 2、公式c另外单独出列 3 double数据类型 /2.0 4.输出内用字母代替

---
+++ 2-3-1-average.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a,b;
+
+    scanf("%d %d",&a,&b);
+
+    double c = (a+b)/2.0;
+
+    printf("%d和%d的平均数是 %f",a,b,c);
+
+    return 0;
+}//
+// Created by xhlly on 2021/10/29.
+//
+
